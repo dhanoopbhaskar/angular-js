3e4162a8cdbe45ded1e939c6c7d68a79b2aea94f
dhanoopbhaskar <dhanoopbhaskar@gmail.com>
2022-06-28 13:26:01 +0530

    log and filter

---
+++ 05.getting-other-services/app.js
@@ -0,0 +1,22 @@
+var myApp = angular.module('myApp', []);
+
+myApp.controller('mainController', function($scope, $log, $filter) {
+
+    console.log($scope);
+    console.log($log);
+
+
+    $log.log("Hello World!");
+    $log.info("This is an information.");
+    $log.debug("This is for debugging.");
+    $log.warn("This is a warning.");
+    $log.error("This is an error.");
+
+
+    $scope.name = 'Dhanoop';
+    $scope.formattedName = $filter('uppercase')($scope.name);
+
+    $log.info($scope.name);
+    $log.info($scope.formattedName);
+
+});
