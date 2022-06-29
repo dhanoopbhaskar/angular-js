4acba6ba32d2f8cad381844ddf093f513281a42b
dhanoopbhaskar <dhanoopbhaskar@gmail.com>
2022-06-29 12:25:37 +0530

    $timeout

--- 07.interpolation/app.js
+++ 07.interpolation/app.js
@@ -1,7 +1,11 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', function($scope) {
+myApp.controller('mainController', ['$scope', '$timeout', function($scope, $timeout) {
     
     $scope.name = 'Dhanoop';
+
+    $timeout(function() {        
+        $scope.name = 'Everybody';
+    }, 3000);
     
 }]);
