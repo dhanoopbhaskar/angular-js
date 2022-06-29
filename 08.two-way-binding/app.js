2515776879e7a3d1c34098bc3d2c7bc166010573
dhanoopbhaskar <dhanoopbhaskar@gmail.com>
2022-06-29 12:42:06 +0530

    change to lowercase

--- 08.two-way-binding/app.js
+++ 08.two-way-binding/app.js
@@ -1,7 +1,11 @@
 var myApp = angular.module('myApp', []);
 
-myApp.controller('mainController', ['$scope', function($scope) {
+myApp.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
     
     $scope.twHandle = '';
+
+    $scope.lowercaseHandle = function() {
+        return $filter('lowercase')($scope.twHandle);
+    };    
     
 }]);
